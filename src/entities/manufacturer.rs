934182258d07ed6b43f7e0bd903289df242a6ad5
damccull <401104+damccull@users.noreply.github.com>
2021-05-26 23:00:59 -0400

    Derive Clone and Default

--- src/entities/manufacturer.rs
+++ src/entities/manufacturer.rs
@@ -1,6 +1,6 @@
 use serde::Deserialize;
 
-#[derive(Debug, Deserialize)]
+#[derive(Clone, Debug, Default, Deserialize)]
 #[serde(rename_all(deserialize = "PascalCase", serialize="camelCase"))]
 pub struct Manufacturer {
     code: String,
