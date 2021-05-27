d4aefe7fd37222ecdebf5ecfabb73bc4ed1deae3
damccull <401104+damccull@users.noreply.github.com>
2021-05-26 23:02:36 -0400

    Test data manipulation in memory

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
-use std::{error::Error, fs::File, io::BufReader, path::Path};
+use std::{collections::HashMap, error::Error, fs::File, io::BufReader, path::Path};
 
 use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 use entities::Ship;
+
+use crate::entities::Manufacturer;
 
 mod entities;
 mod verify_ed25519_signature;
@@ -20,7 +22,27 @@ async fn api(req: HttpRequest) -> impl Responder {
 async fn main() -> std::io::Result<()> {
     match read_ships_from_file("ships.json") {
         Ok(ships) => {
-            dbg!(ships);
+            let ship_class_names: Vec<String> =
+                ships.iter().map(|s| s.class_name.clone()).collect();
+            let manufacturers: Vec<Manufacturer> =
+                ships.iter().fold(Vec::<Manufacturer>::new(), |mut acc, s| {
+                    if acc.iter().find(|m| m.name == s.manufacturer.name).is_none() {
+                        acc.push(s.manufacturer.clone());
+                    }
+                    acc
+                });
+            let ships_by_manufacturer: HashMap<String, Vec<String>> =
+                ships
+                    .into_iter()
+                    .fold(HashMap::<String, Vec<String>>::new(), |mut acc, ship| {
+                        acc.entry(ship.manufacturer.name.clone())
+                            .or_insert_with(Vec::<String>::new)
+                            .push(ship.name);
+                        acc
+                    });
+            let query = find_manufacturer_by_code(manufacturers, &String::from("cn"));
+            dbg!(query);
+            // dbg!(ships_by_manufacturer.get_key_value("Anvil"));
         }
         Err(e) => panic!("Error reading ships.json: {:?}", e),
     }
@@ -38,6 +60,22 @@ async fn main() -> std::io::Result<()> {
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
+}
+
+fn find_manufacturer_by_name(manufacturers: Vec<Manufacturer>, query: &str) -> Vec<Manufacturer> {
+    let query: Vec<Manufacturer> = manufacturers
+        .into_iter()
+        .filter(|m| m.name.to_lowercase().contains(&query.to_lowercase()))
+        .collect();
+    query
+}
+
+fn find_manufacturer_by_code(manufacturers: Vec<Manufacturer>, query: &str) -> Vec<Manufacturer> {
+    let query: Vec<Manufacturer> = manufacturers
+        .into_iter()
+        .filter(|m| m.code.to_lowercase().contains(&query.to_lowercase()))
+        .collect();
+    query
 }
 
 fn read_ships_from_file<P: AsRef<Path>>(path: P) -> Result<Vec<Ship>, Box<dyn Error>> {
