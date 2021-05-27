78e0a173b4ee1ce8ef99f222bc14c15564edd4d6
damccull <401104+damccull@users.noreply.github.com>
2021-05-26 23:02:23 -0400

    Make properties pub

--- src/entities/manufacturer.rs
+++ src/entities/manufacturer.rs
@@ -3,6 +3,6 @@ use serde::Deserialize;
 #[derive(Clone, Debug, Default, Deserialize)]
 #[serde(rename_all(deserialize = "PascalCase", serialize="camelCase"))]
 pub struct Manufacturer {
-    code: String,
-    name: String,
+    pub code: String,
+    pub name: String,
 }
