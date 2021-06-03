daf2f24714f8f9dfba532059c8ae9b7cf3cd283f
damccull <401104+damccull@users.noreply.github.com>
2021-06-02 23:27:34 -0400

    Remove 'greet' route

--- src/lib.rs
+++ src/lib.rs
@@ -12,8 +12,6 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
             .route("/health_check", web::get().to(health_check))
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
             .service(
                 web::scope("/api").service(
                     web::scope("/discord")
@@ -31,11 +29,6 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
 
 async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
-}
-
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
 }
 
 async fn api(req: HttpRequest) -> impl Responder {
