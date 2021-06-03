6b6a993cf08d9e5b9a996b6ba3594d6bd26d8c54
damccull <401104+damccull@users.noreply.github.com>
2021-06-03 00:08:06 -0400

    formatting

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,11 @@
 
 use std::net::TcpListener;
 
-use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, dev::Server, web::{self, scope}};
+use actix_web::{
+    dev::Server,
+    web::{self, scope},
+    App, HttpRequest, HttpResponse, HttpServer, Responder,
+};
 
 mod crypto;
 mod entities;
@@ -15,17 +19,18 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .service(
-                web::scope("/api").service(
-                    web::scope("/discord")
-                        .wrap(crypto::VerifyEd25519Signature)
-                        .route("/{interaction}", web::get().to(api)),
-                )
-                .route("/{interaction}", web::get().to(api)),
+                web::scope("/api")
+                    .service(
+                        web::scope("/discord")
+                            .wrap(crypto::VerifyEd25519Signature)
+                            .route("/{interaction}", web::get().to(api)),
+                    )
+                    .route("/{interaction}", web::get().to(api)),
             )
     })
     .listen(listener)?
     .run();
-    
+
     Ok(server)
 }
 
@@ -34,6 +39,11 @@ async fn health_check() -> HttpResponse {
 }
 
 async fn api(req: HttpRequest) -> impl Responder {
-    let interaction = req.match_info().get("interaction").unwrap_or("bad api");
-    format!("API requested path: {}", &interaction)
+    let interaction = match req.match_info().get("interaction") {
+        Some(value) => value,
+        None => {
+            return HttpResponse::NotFound().finish();
+        }
+    };
+    HttpResponse::Ok().body(format!("API requested path: {}", &interaction))
 }
