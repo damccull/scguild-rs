9c2cc984e1e74720a88ee344e40677e2b4f6cdb7
damccull <401104+damccull@users.noreply.github.com>
2021-06-03 21:56:34 -0400

    Make id column not null

--- migrations/2021-06-04-000927_initial/up.sql
+++ migrations/2021-06-04-000927_initial/up.sql
@@ -1,6 +1,6 @@
 -- manufacturers table
 CREATE TABLE manufacturers (
-    id           INTEGER PRIMARY KEY AUTOINCREMENT,
+    id           INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     name         TEXT    NOT NULL,
     abbreviation TEXT    NOT NULL,
     description  TEXT
@@ -18,7 +18,7 @@ CREATE INDEX manufacturers_abbreviation_idx ON manufacturers (
 -- ship_blueprints table
 
 CREATE TABLE ship_blueprints (
-    id              INTEGER PRIMARY KEY AUTOINCREMENT,
+    id              INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     name            TEXT    NOT NULL,
     manufacturer_id INTEGER NOT NULL,
     description     TEXT,
@@ -37,7 +37,7 @@ CREATE INDEX ship_blueprints_name_idx ON ship_blueprints (
 -- ship_blueprint_classifications table
 
 CREATE TABLE ship_blueprint_classifications (
-    id          INTEGER PRIMARY KEY AUTOINCREMENT,
+    id          INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     name        TEXT    NOT NULL,
     description TEXT
 );
@@ -54,7 +54,7 @@ CREATE INDEX ship_blueprint_classifications_name_IDX ON ship_blueprint_classific
 -- ship_blueprint_variants table
 
 CREATE TABLE ship_blueprint_variants (
-    id                INTEGER PRIMARY KEY AUTOINCREMENT,
+    id                INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     name              TEXT    NOT NULL,
     ship_blueprint_id INTEGER NOT NULL,
     description       TEXT,
@@ -81,7 +81,7 @@ CREATE INDEX ship_blueprint_variants_ship_blueprint_id_IDX ON ship_blueprint_var
 -- ship_blueprint_variant_join_classification table
 
 CREATE TABLE ship_blueprint_variant_join_classification (
-    id                               INTEGER PRIMARY KEY AUTOINCREMENT,
+    id                               INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     ship_blueprint_variant_id        INTEGER NOT NULL,
     ship_blueprint_classification_id INTEGER NOT NULL,
     CONSTRAINT ship_blueprint_variant_join_classification_UN UNIQUE (
