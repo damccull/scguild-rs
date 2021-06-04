9966941350644ed424103239820defc701ec2e88
damccull <401104+damccull@users.noreply.github.com>
2021-06-03 20:24:11 -0400

    Ignore database files

---
+++ migrations/2021-06-04-000927_initial/up.sql
@@ -0,0 +1,113 @@
+-- manufacturers table
+CREATE TABLE manufacturers (
+    id           INTEGER PRIMARY KEY AUTOINCREMENT,
+    name         TEXT    NOT NULL,
+    abbreviation TEXT    NOT NULL,
+    description  TEXT
+);
+
+CREATE INDEX manufacturers_name_idx ON manufacturers (
+    name
+);
+
+CREATE INDEX manufacturers_abbreviation_idx ON manufacturers (
+    abbreviation
+);
+
+
+-- ship_blueprints table
+
+CREATE TABLE ship_blueprints (
+    id              INTEGER PRIMARY KEY AUTOINCREMENT,
+    name            TEXT    NOT NULL,
+    manufacturer_id INTEGER NOT NULL,
+    description     TEXT,
+    CONSTRAINT ship_blueprints_FK FOREIGN KEY (
+        manufacturer_id
+    )
+    REFERENCES manufacturers (id) ON DELETE RESTRICT
+                                  ON UPDATE RESTRICT
+);
+
+CREATE INDEX ship_blueprints_name_idx ON ship_blueprints (
+    name
+);
+
+
+-- ship_blueprint_classifications table
+
+CREATE TABLE ship_blueprint_classifications (
+    id          INTEGER PRIMARY KEY AUTOINCREMENT,
+    name        TEXT    NOT NULL,
+    description TEXT
+);
+
+CREATE INDEX ship_blueprint_classifications_id_IDX ON ship_blueprint_classifications (
+    id
+);
+
+CREATE INDEX ship_blueprint_classifications_name_IDX ON ship_blueprint_classifications (
+    name
+);
+
+
+-- ship_blueprint_variants table
+
+CREATE TABLE ship_blueprint_variants (
+    id                INTEGER PRIMARY KEY AUTOINCREMENT,
+    name              TEXT    NOT NULL,
+    ship_blueprint_id INTEGER NOT NULL,
+    description       TEXT,
+    CONSTRAINT ship_blueprint_variants_FK FOREIGN KEY (
+        ship_blueprint_id
+    )
+    REFERENCES ship_blueprints (id) ON DELETE RESTRICT
+                                    ON UPDATE RESTRICT
+);
+
+CREATE INDEX ship_blueprint_variants_id_IDX ON ship_blueprint_variants (
+    id
+);
+
+CREATE INDEX ship_blueprint_variants_name_IDX ON ship_blueprint_variants (
+    name
+);
+
+CREATE INDEX ship_blueprint_variants_ship_blueprint_id_IDX ON ship_blueprint_variants (
+    ship_blueprint_id
+);
+
+
+-- ship_blueprint_variant_join_classification table
+
+CREATE TABLE ship_blueprint_variant_join_classification (
+    id                               INTEGER PRIMARY KEY AUTOINCREMENT,
+    ship_blueprint_variant_id        INTEGER NOT NULL,
+    ship_blueprint_classification_id INTEGER NOT NULL,
+    CONSTRAINT ship_blueprint_variant_join_classification_UN UNIQUE (
+        ship_blueprint_variant_id,
+        ship_blueprint_classification_id
+    ),
+    CONSTRAINT ship_blueprint_variant_join_classification_FK FOREIGN KEY (
+        ship_blueprint_variant_id
+    )
+    REFERENCES ship_blueprint_variants (id) ON DELETE RESTRICT
+                                            ON UPDATE RESTRICT,
+    CONSTRAINT ship_blueprint_variant_join_classification_FK_1 FOREIGN KEY (
+        ship_blueprint_classification_id
+    )
+    REFERENCES ship_blueprint_classifications (id) ON DELETE RESTRICT
+                                                   ON UPDATE RESTRICT
+);
+
+CREATE INDEX ship_blueprint_variant_join_classification_id_IDX ON ship_blueprint_variant_join_classification (
+    id
+);
+
+CREATE INDEX ship_blueprint_variant_join_classification_ship_blueprint_classification_id_IDX ON ship_blueprint_variant_join_classification (
+    ship_blueprint_classification_id
+);
+
+CREATE INDEX ship_blueprint_variant_join_classification_ship_blueprint_variant_id_IDX ON ship_blueprint_variant_join_classification (
+    ship_blueprint_variant_id
+);
---
+++ src/schema.rs
@@ -0,0 +1,55 @@
+table! {
+    manufacturers (id) {
+        id -> Nullable<Integer>,
+        name -> Text,
+        abbreviation -> Text,
+        description -> Nullable<Text>,
+    }
+}
+
+table! {
+    ship_blueprint_classifications (id) {
+        id -> Nullable<Integer>,
+        name -> Text,
+        description -> Nullable<Text>,
+    }
+}
+
+table! {
+    ship_blueprint_variant_join_classification (id) {
+        id -> Nullable<Integer>,
+        ship_blueprint_variant_id -> Integer,
+        ship_blueprint_classification_id -> Integer,
+    }
+}
+
+table! {
+    ship_blueprint_variants (id) {
+        id -> Nullable<Integer>,
+        name -> Text,
+        ship_blueprint_id -> Integer,
+        description -> Nullable<Text>,
+    }
+}
+
+table! {
+    ship_blueprints (id) {
+        id -> Nullable<Integer>,
+        name -> Text,
+        manufacturer_id -> Integer,
+        description -> Nullable<Text>,
+    }
+}
+
+joinable!(ship_blueprint_variant_join_classification -> ship_blueprint_classifications (ship_blueprint_classification_id));
+joinable!(ship_blueprint_variant_join_classification -> ship_blueprint_variants (ship_blueprint_variant_id));
+joinable!(ship_blueprint_variants -> ship_blueprints (ship_blueprint_id));
+joinable!(ship_blueprints -> manufacturers (manufacturer_id));
+
+allow_tables_to_appear_in_same_query!(
+    manufacturers,
+    ship_blueprint_classifications,
+    ship_blueprint_variant_join_classification,
+    ship_blueprint_variants,
+    ship_blueprints,
+);
