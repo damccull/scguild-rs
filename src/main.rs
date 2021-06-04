1c709e413397b57d16d85ddc51a4f27a7160ab7b
damccull <401104+damccull@users.noreply.github.com>
2021-06-03 21:57:59 -0400

    Catch up to changes

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,27 @@
 #![allow(unused)]
 
+#[macro_use]
+extern crate diesel;
+
 use std::{collections::HashMap, error::Error, fs::File, io::BufReader, net::TcpListener, path::Path};
 
 use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 use entities::Ship;
+use tokio::sync::mpsc;
 
 use crate::entities::Manufacturer;
 
 mod crypto;
 mod entities;
-mod database;
+mod database_actor;
+mod db;
 
 use norseline::run;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    test_stuff();
-
+    //test_stuff();
+    test_db();
 
     //------- TEMPORARILY DISABLED: This enables the web server
     // Create a TcpListener to pass to the server. This allows for easy integration testing.
@@ -25,8 +30,14 @@ async fn main() -> std::io::Result<()> {
     Ok(())
 }
 
+fn test_db() {
+    let (tx, rx) = mpsc::channel(5);
+    let mut ac = database_actor::DatabaseActor::new(rx);
+    ac.handle_message(database_actor::DatabaseMessage::GetManufacturers);
+}
+
 fn test_stuff() {
-    match database::read_ships_from_file("ships.json") {
+    match database_actor::read_ships_from_file("ships.json") {
         Ok(ships) => {
             let ship_class_names: Vec<String> =
                 ships.iter().map(|s| s.class_name.clone()).collect();
@@ -47,7 +58,7 @@ fn test_stuff() {
                         acc
                     });
             //let query = find_manufacturer_by_code(manufacturers, &String::from("cn"));
-            let query = database::find_ship_by_name(ships, "stalker");
+            let query = database_actor::find_ship_by_name(ships, "stalker");
             dbg!(query);
             // dbg!(ships_by_manufacturer.get_key_value("Anvil"));
         }
