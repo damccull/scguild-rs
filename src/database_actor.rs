98f32322e9d0f77d9db200ddd30677b04806cdd2
damccull <401104+damccull@users.noreply.github.com>
2021-06-05 19:50:29 -0400

    Begin working on actual database actor

--- src/database_actor.rs
+++ src/database_actor.rs
@@ -3,33 +3,37 @@ use std::{env, error::Error, fs::File, io::BufReader, path::Path};
 use diesel::prelude::*;
 use tokio::sync::{mpsc, oneshot};
 
+use crate::db::models::Manufacturer;
 use crate::{db::models as dbmodels, entities};
 
 use crate::db::schema::manufacturers::dsl::manufacturers as manufacturer_dsl;
 
 pub enum DatabaseMessage {
-    GetManufacturers,
+    GetManufacturers {
+        respond_to: oneshot::Sender<Vec<Manufacturer>>,
+    },
 }
 
 pub struct DatabaseActor {
     pub receiver: mpsc::Receiver<DatabaseMessage>,
+    sqlite_connection: SqliteConnection,
 }
 
 impl DatabaseActor {
     pub fn new(receiver: mpsc::Receiver<DatabaseMessage>) -> Self {
-        DatabaseActor { receiver }
+        DatabaseActor {
+            receiver,
+            sqlite_connection: Self::establish_connection(),
+        }
     }
 
     pub fn handle_message(&mut self, msg: DatabaseMessage) {
         match msg {
-            DatabaseMessage::GetManufacturers => {
-                let connection = Self::establish_connection();
-
+            DatabaseMessage::GetManufacturers { respond_to } => {
                 let results = manufacturer_dsl
-                    .load::<dbmodels::Manufacturer>(&connection)
+                    .load::<dbmodels::Manufacturer>(&self.sqlite_connection)
                     .expect("Error loading manufacturers.");
-
-                dbg!(results);
+                let _ = respond_to.send(results);
             }
         }
     }
@@ -58,13 +62,37 @@ async fn run_database_actor(mut actor: DatabaseActor) {
     }
 }
 
-//----------------
-pub fn find_ship_by_name(ships: Vec<entities::Ship>, query: &str) -> Vec<entities::Ship> {
-    ships
-        .into_iter()
-        .filter(|s| s.name.to_lowercase().contains(&query.to_lowercase()))
-        .collect()
+#[derive(Clone)]
+pub struct DatabaseActorHandle {
+    sender: mpsc::Sender<DatabaseMessage>,
 }
+
+impl DatabaseActorHandle {
+    pub fn new() -> Self {
+        let (sender, receiver) = mpsc::channel(8);
+        let actor = DatabaseActor::new(receiver);
+
+        tokio::spawn(run_database_actor(actor));
+
+        Self { sender }
+    }
+
+    pub async fn get_all_manufacturers(& self) -> Vec<Manufacturer> {
+        let (send, receive) = oneshot::channel();
+        let msg = DatabaseMessage::GetManufacturers { respond_to: send };
+
+        let _ = self.sender.send(msg).await;
+        receive.await.expect("Actor task has been killed")
+    }
+}
+
+//----------------
+// pub fn find_ship_by_name(ships: Vec<entities::Ship>, query: &str) -> Vec<entities::Ship> {
+//     ships
+//         .into_iter()
+//         .filter(|s| s.name.to_lowercase().contains(&query.to_lowercase()))
+//         .collect()
+// }
 // pub fn find_manufacturer_by_name(
 //     manufacturers: Vec<entities::Manufacturer>,
 //     query: &str,
@@ -85,16 +113,16 @@ pub fn find_ship_by_name(ships: Vec<entities::Ship>, query: &str) -> Vec<entitie
 //         .collect();
 //     query
 // }
-pub fn read_ships_from_file<P: AsRef<Path>>(
-    path: P,
-) -> Result<Vec<entities::Ship>, Box<dyn Error>> {
-    // Open the file in read-only mode with buffer.
-    let file = File::open(path)?;
-    let reader = BufReader::new(file);
+// pub fn read_ships_from_file<P: AsRef<Path>>(
+//     path: P,
+// ) -> Result<Vec<entities::Ship>, Box<dyn Error>> {
+//     // Open the file in read-only mode with buffer.
+//     let file = File::open(path)?;
+//     let reader = BufReader::new(file);
 
-    // Read the JSON contents of the file as an instance of `User`.
-    let ships = serde_json::from_reader(reader)?;
+//     // Read the JSON contents of the file as an instance of `User`.
+//     let ships = serde_json::from_reader(reader)?;
 
-    // Return the `User`.
-    Ok(ships)
-}
+//     // Return the `User`.
+//     Ok(ships)
+// }
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ extern crate diesel;
 use std::{collections::HashMap, error::Error, fs::File, io::BufReader, net::TcpListener, path::Path};
 
 use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+use database_actor::DatabaseActorHandle;
 use entities::Ship;
 use tokio::sync::mpsc;
 
@@ -21,7 +22,7 @@ use norseline::run;
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     //test_stuff();
-    test_db();
+    test_db().await;
 
     //------- TEMPORARILY DISABLED: This enables the web server
     // Create a TcpListener to pass to the server. This allows for easy integration testing.
@@ -30,39 +31,38 @@ async fn main() -> std::io::Result<()> {
     Ok(())
 }
 
-fn test_db() {
-    let (tx, rx) = mpsc::channel(5);
-    let mut ac = database_actor::DatabaseActor::new(rx);
-    ac.handle_message(database_actor::DatabaseMessage::GetManufacturers);
+async fn test_db() {
+    let dbhandle = DatabaseActorHandle::new();
+    dbg!(dbhandle.get_all_manufacturers().await);
 }
 
-fn test_stuff() {
-    match database_actor::read_ships_from_file("ships.json") {
-        Ok(ships) => {
-            let ship_class_names: Vec<String> =
-                ships.iter().map(|s| s.class_name.clone()).collect();
-            let manufacturers: Vec<Manufacturer> =
-                ships.iter().fold(Vec::<Manufacturer>::new(), |mut acc, s| {
-                    if acc.iter().find(|m| m.name == s.manufacturer.name).is_none() {
-                        acc.push(s.manufacturer.clone());
-                    }
-                    acc
-                });
-            let ships_by_manufacturer: HashMap<String, Vec<String>> =
-                ships
-                    .iter()
-                    .fold(HashMap::<String, Vec<String>>::new(), |mut acc, ship| {
-                        acc.entry(ship.manufacturer.name.clone())
-                            .or_insert_with(Vec::<String>::new)
-                            .push(ship.name.clone());
-                        acc
-                    });
-            //let query = find_manufacturer_by_code(manufacturers, &String::from("cn"));
-            let query = database_actor::find_ship_by_name(ships, "stalker");
-            dbg!(query);
-            // dbg!(ships_by_manufacturer.get_key_value("Anvil"));
-        }
-        Err(e) => panic!("Error reading ships.json: {:?}", e),
-    }
-}
+// fn test_stuff() {
+//     match database_actor::read_ships_from_file("ships.json") {
+//         Ok(ships) => {
+//             let ship_class_names: Vec<String> =
+//                 ships.iter().map(|s| s.class_name.clone()).collect();
+//             let manufacturers: Vec<Manufacturer> =
+//                 ships.iter().fold(Vec::<Manufacturer>::new(), |mut acc, s| {
+//                     if acc.iter().find(|m| m.name == s.manufacturer.name).is_none() {
+//                         acc.push(s.manufacturer.clone());
+//                     }
+//                     acc
+//                 });
+//             let ships_by_manufacturer: HashMap<String, Vec<String>> =
+//                 ships
+//                     .iter()
+//                     .fold(HashMap::<String, Vec<String>>::new(), |mut acc, ship| {
+//                         acc.entry(ship.manufacturer.name.clone())
+//                             .or_insert_with(Vec::<String>::new)
+//                             .push(ship.name.clone());
+//                         acc
+//                     });
+//             //let query = find_manufacturer_by_code(manufacturers, &String::from("cn"));
+//             let query = database_actor::find_ship_by_name(ships, "stalker");
+//             dbg!(query);
+//             // dbg!(ships_by_manufacturer.get_key_value("Anvil"));
+//         }
+//         Err(e) => panic!("Error reading ships.json: {:?}", e),
+//     }
+// }
 
