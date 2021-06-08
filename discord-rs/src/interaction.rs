3407cfb5d4bd49801c09abcf5faac12ad796572b
damccull <401104+damccull@users.noreply.github.com>
2021-06-08 14:04:22 -0400

    Remove unneeded test.

--- discord-rs/src/interaction.rs
+++ discord-rs/src/interaction.rs
@@ -99,11 +99,3 @@ pub enum InteractionType {
     ApplicationCommand = 2,
     MessageComponent = 3,
 }
-
-// #[cfg(test)]
-// mod tests {
-//     #[test]
-//     fn it_works() {
-//         assert_eq!(2 + 2, 4);
-//     }
-// }
