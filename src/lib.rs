060ced89357b0682235ee64b9b504bc6a216a489
damccull <401104+damccull@users.noreply.github.com>
2021-12-08 16:01:28 -0500

    Make PING and DEBUG command interactions work

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,17 @@ pub mod middleware;
 pub mod routes;
 pub mod serde_helpers;
 pub mod telemetry;
+
+
+pub fn error_chain_fmt(
+    e: &impl std::error::Error,
+    f: &mut std::fmt::Formatter<'_>,
+) -> std::fmt::Result {
+    writeln!(f, "{}\n", e)?;
+    let mut current = e.source();
+    while let Some(cause) = current {
+        writeln!(f, "Cause by:\n\t{}", cause)?;
+        current = cause.source();
+    }
+    Ok(())
+}
--- src/routes/discord_api.rs
+++ src/routes/discord_api.rs
@@ -1,12 +1,78 @@
-use actix_web::{http::header, web, HttpRequest, HttpResponse, Responder};
-use twilight_model::application::{callback::InteractionResponse, interaction::Interaction};
+use actix_web::{http::header, web, HttpRequest, HttpResponse, ResponseError};
+use anyhow::Context;
+use twilight_model::application::{
+    callback::{CallbackData, InteractionResponse},
+    interaction::Interaction,
+};
 
-pub async fn discord_api(_req: HttpRequest, interaction: web::Json<Interaction>) -> impl Responder {
+use crate::error_chain_fmt;
+
+#[tracing::instrument(name = "calling discord api", skip(_req))]
+pub async fn discord_api(
+    _req: HttpRequest,
+    interaction: web::Json<Interaction>,
+) -> Result<HttpResponse, DiscordApiError> {
     match interaction.0 {
-        Interaction::Ping(_) => HttpResponse::Ok()
+        Interaction::Ping(_) => Ok(HttpResponse::Ok()
             .append_header(header::ContentType(mime::APPLICATION_JSON))
-            .json(InteractionResponse::Pong),
-        Interaction::ApplicationCommand(_) => HttpResponse::Ok().finish(),
-        _ => HttpResponse::BadRequest().finish(),
+            .json(InteractionResponse::Pong)),
+        Interaction::ApplicationCommand(_) => {
+            // Run handler to get correct response
+            let response = application_command_handler(interaction.0)
+                .await
+                .context("Problem running application command handler")?;
+            Ok(HttpResponse::Ok()
+                .append_header(header::ContentType(mime::APPLICATION_JSON))
+                .json(response))
+        }
+        _ => Err(DiscordApiError::UnexpectedError(anyhow::anyhow!(
+            "Bad interaction".to_string()
+        ))),
+    }
+}
+
+#[tracing::instrument(name = "application_command_handler")]
+async fn application_command_handler(
+    interaction: Interaction,
+) -> Result<InteractionResponse, DiscordApiError> {
+    match interaction {
+        Interaction::ApplicationCommand(ref cmd) => match cmd.data.name.as_ref() {
+            "debug" => debug(interaction).await,
+            _ => debug(interaction).await,
+        },
+        _ => Err(DiscordApiError::UnexpectedError(anyhow::anyhow!(
+            "Invalid interaction data".to_string()
+        ))),
+    }
+}
+
+async fn debug(interaction: Interaction) -> Result<InteractionResponse, DiscordApiError> {
+    Ok(InteractionResponse::ChannelMessageWithSource(
+        CallbackData {
+            allowed_mentions: None,
+            flags: None,
+            tts: None,
+            content: Some(format!("```rust\n{:?}\n```", interaction)),
+            embeds: Default::default(),
+            components: Default::default(),
+        },
+    ))
+}
+
+#[derive(thiserror::Error)]
+pub enum DiscordApiError {
+    #[error(transparent)]
+    UnexpectedError(#[from] anyhow::Error),
+}
+impl std::fmt::Debug for DiscordApiError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        error_chain_fmt(self, f)
+    }
+}
+impl ResponseError for DiscordApiError {
+    fn status_code(&self) -> actix_http::StatusCode {
+        match self {
+            DiscordApiError::UnexpectedError(_) => actix_http::StatusCode::INTERNAL_SERVER_ERROR,
+        }
     }
 }
