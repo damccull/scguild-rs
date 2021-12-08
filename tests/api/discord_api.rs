793b1f540c572c9fb57948825d0426764d18d1a1
damccull <401104+damccull@users.noreply.github.com>
2021-12-08 16:01:34 -0500

    Add commented out test

--- tests/api/discord_api.rs
+++ tests/api/discord_api.rs
@@ -52,9 +52,49 @@ async fn discord_api_responds_with_pong_when_given_ping() {
     }
 
     let json = response.json::<Pong>().await;
-    dbg!(&json);
 
     // Assert
     assert_eq!(status, StatusCode::OK);
     assert_eq!(json.unwrap().interaction_type, 1_u8);
 }
+
+// #[actix_rt::test]
+// async fn discord_api_responds_with_debug_when_debug_interaction_sent() {
+//     // Arrange
+//     let test_app = TestApp::spawn().await;
+//     let keypair = test_app.discord_keypair;
+
+//     let body = interaction::ApplicationCommand{
+
+//     }
+
+//     let x = serde_json::to_string(&body).unwrap();
+
+//     let timestamp = Utc::now().to_string();
+//     let signme = format!("{}{}", timestamp.to_string(), x);
+//     let signature = keypair.sign(signme.as_bytes());
+
+//     // Act
+//     let response = reqwest::Client::new()
+//         .post(format!("{}/api/discord", &test_app.address))
+//         .header("X-Signature-Ed25519", signature.to_string())
+//         .header("X-Signature-Timestamp", timestamp)
+//         .json(&body)
+//         .send()
+//         .await
+//         .expect("Failed to execute request.");
+
+//     let status = response.status();
+
+//     #[derive(Debug, Deserialize)]
+//     struct Pong {
+//         #[serde(rename = "type")]
+//         pub interaction_type: u8,
+//     }
+
+//     let json = response.json::<Pong>().await;
+
+//     // Assert
+//     assert_eq!(status, StatusCode::OK);
+//     assert_eq!(json.unwrap().interaction_type, 1_u8);
+// }
