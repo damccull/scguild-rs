7159518bc1a19511f9ec0a849be72806242210b3
damccull <401104+damccull@users.noreply.github.com>
2021-12-10 01:19:24 -0500

    Build a basic command template for fleet

--- src/discord/commands/fleet.rs
+++ src/discord/commands/fleet.rs
@@ -1,10 +1,10 @@
 use async_trait::async_trait;
 use twilight_model::application::{
     callback::{CallbackData, InteractionResponse},
-    command::{Command, CommandType},
-    interaction::{ApplicationCommand, Interaction},
+    command::{ChoiceCommandOptionData, Command, CommandType},
+    interaction::{self, ApplicationCommand, Interaction},
 };
-use twilight_util::builder::command::CommandBuilder;
+use twilight_util::builder::command::{CommandBuilder, SubCommandBuilder, SubCommandGroupBuilder};
 
 use crate::discord::{api::DiscordApiError, SlashCommand};
 
@@ -20,11 +20,58 @@ impl SlashCommand for Fleet {
             "Displays and manages a player's fleet.".into(),
             CommandType::ChatInput,
         )
+        .option(
+            SubCommandGroupBuilder::new("hangar".into(), "Manage ships in your fleet".into())
+                .subcommands([
+                    SubCommandBuilder::new(
+                        "list".into(),
+                        "List all the ships in your (or someone else's) fleet privately.".into(),
+                    ),
+                    SubCommandBuilder::new(
+                        "show".into(),
+                        "Show the whole channel the ships in your fleet.".into(),
+                    ),
+                    SubCommandBuilder::new("add".into(), "Add a new ship to your fleet.".into()),
+                    SubCommandBuilder::new(
+                        "remove".into(),
+                        "Remove a ship from your fleet.".into(),
+                    ),
+                    SubCommandBuilder::new(
+                        "rename".into(),
+                        "Remove a ship from your fleet.".into(),
+                    ),
+                ]),
+        )
+        .option(
+            SubCommandGroupBuilder::new("wishlist".into(), "Manage ships in your wishlist".into())
+                .subcommands([
+                    SubCommandBuilder::new(
+                        "list".into(),
+                        "List all the ships in your (or someone else's) wishlist privately.".into(),
+                    ),
+                    SubCommandBuilder::new(
+                        "show".into(),
+                        "Show the whole channel the ships in your wishlist.".into(),
+                    ),
+                    SubCommandBuilder::new("add".into(), "Add a new ship to your wishlist.".into()),
+                    SubCommandBuilder::new(
+                        "remove".into(),
+                        "Remove a ship from your wishlist.".into(),
+                    ),
+                    SubCommandBuilder::new(
+                        "rename".into(),
+                        "Remove a ship from your wishlist.".into(),
+                    ),
+                ]),
+        )
         .build()
     }
 
     #[tracing::instrument(name = "Discord Interaction - FLEET")]
-    async fn api_handler(_: Interaction) -> Result<InteractionResponse, DiscordApiError> {
+    async fn api_handler(
+        cmd: twilight_model::application::interaction::Interaction,
+    ) -> Result<InteractionResponse, DiscordApiError> {
+        let x = cmd;
         Ok(InteractionResponse::ChannelMessageWithSource(
             CallbackData {
                 allowed_mentions: None,
