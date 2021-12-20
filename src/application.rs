ad2fcd03cc3eb4509bc30768c66c2ebb164c2723
damccull <401104+damccull@users.noreply.github.com>
2021-12-20 17:00:42 -0500

    Change the logging level of some messages.

--- src/application.rs
+++ src/application.rs
@@ -83,7 +83,7 @@ impl Application {
         }
         let reqwestclient = reqwest::Client::new();
 
-        tracing::info!("Getting client credential.");
+        tracing::debug!("Getting client credential.");
 
         let params = [
             ("grant_type", "client_credentials"),
@@ -107,15 +107,15 @@ impl Application {
             .await
             .context("Error requesting client credential from Discord API")?;
 
-        tracing::info!("Deserializing client credential.");
+        tracing::debug!("Deserializing client credential.");
 
         let client_credential = client_credential
             .json::<ClientCredential>()
             .await
             .context("Error deserializing client credential")?;
 
-        tracing::info!("Client credential is valid.");
-        tracing::info!("Setting application commands with discord API.");
+        tracing::debug!("Client credential is valid.");
+        tracing::debug!("Setting application commands with discord API.");
 
         let http = Arc::new(HttpClient::new(format!(
             "Bearer {}",
@@ -127,14 +127,14 @@ impl Application {
         // // http.set_global_commands(&discord_commands::commands())?
         // //     .exec()
         // //     .await?;
-        tracing::info!("Setting guild commands.");
+        tracing::debug!("Setting guild commands.");
         http.set_guild_commands(
             GuildId::new(self.discord_settings.guild_id).unwrap(),
             &discord::commands(),
         )?
         .exec()
         .await?;
-        tracing::info!("Guild commands set.");
+        tracing::info!("Guild commands registered.");
 
         // REMOVE COMMANDS
         // http.set_global_commands(&[])?.exec().await?;
