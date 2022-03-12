f2c13e919c929c5413a4a65c7c5d1e607f16d861
damccull <401104+damccull@users.noreply.github.com>
2022-03-11 21:11:06 -0500

    Change the test url to match the new server url.

--- tests/api/discord_api.rs
+++ tests/api/discord_api.rs
@@ -31,7 +31,7 @@ async fn discord_api_responds_with_pong_when_given_ping() {
 
     // Act
     let response = reqwest::Client::new()
-        .post(format!("{}/api/discord", &test_app.address))
+        .post(format!("{}/discord", &test_app.address))
         .header("X-Signature-Ed25519", signature.to_string())
         .header("X-Signature-Timestamp", timestamp)
         .json(&body)
