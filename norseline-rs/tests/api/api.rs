78f68f5be86d73df273400e8f399107b1b41d08d
damccull <401104+damccull@users.noreply.github.com>
2022-05-03 08:43:30 -0400

    Change some comments.

--- norseline-rs/tests/api/api.rs
+++ norseline-rs/tests/api/api.rs
@@ -2,37 +2,37 @@
 
 #[actix_rt::test]
 async fn api_sends_404_with_missing_endpoint() {
-    // Arrange test
+    // Arrange
     let address = common::spawn_app();
-    // Create a reqwest client
+
     let client = reqwest::Client::new();
 
-    // Do the actions
+    // Act
     let response = client
         .get(&format!("{}/api/missingendpoint", address.as_str()))
         .send()
         .await
         .expect("Failed to execute request.");
 
-    //Run assertions against the data
+    // Assert
     assert_eq!(response.status().as_u16(), 404);
 }
 
 #[actix_rt::test]
 async fn api_sends_200_with_correct_endpoint() {
-    // Arrange test
+    // Arrange
     let address = common::spawn_app();
-    // Create a reqwest client
+
     let client = reqwest::Client::new();
 
-    // Do the actions
+    // Act
     let response = client
         .get(&format!("{}/api/v1/testing", address.as_str()))
         .send()
         .await
         .expect("Failed to execute request.");
 
-    //Run assertions against the data
+    // Assert
     assert_eq!(response.status().as_u16(), 200);
     assert_eq!(
         response.text().await.unwrap(),
