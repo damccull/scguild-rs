eb819679d5b57a9f64fd3da931e56fbeee8bd2ab
damccull <401104+damccull@users.noreply.github.com>
2022-05-23 20:14:00 -0400

    Update ci xtask to allow colors through from underlying files

--- xtask/src/tasks/ci.rs
+++ xtask/src/tasks/ci.rs
@@ -1,46 +1,63 @@
-use std::process::Command;
+use colored::Colorize;
+use std::process::{Command, ExitStatus};
 
 use crate::{project_root, DynError};
 
 pub fn ci() -> Result<(), DynError> {
     println!("Running `cargo check`...");
-    let check = Command::new("cargo").args(["check"]).output()?;
-    println!("{}", std::str::from_utf8(&check.stdout)?);
-    println!("{}", std::str::from_utf8(&check.stderr)?);
+    let check = Command::new("cargo")
+        .args(["check", "-p", "norseline"])
+        .status()?;
 
     println!("Running `cargo clippy`...");
-    let clippy = Command::new("cargo").args(["clippy"]).output()?;
-    println!("{}", std::str::from_utf8(&clippy.stdout)?);
-    println!("{}", std::str::from_utf8(&clippy.stderr)?);
+    let clippy = Command::new("cargo")
+        .args(["clippy", "-p", "norseline"])
+        .status()?;
 
     println!("Running `cargo build`...");
-    let build = Command::new("cargo").args(["build"]).output()?;
-    println!("{}", std::str::from_utf8(&build.stdout)?);
-    println!("{}", std::str::from_utf8(&build.stderr)?);
+    let build = Command::new("cargo")
+        .args(["build", "-p", "norseline"])
+        .status()?;
 
     println!("Running `cargo test`...");
-    let test = Command::new("cargo").args(["test"]).output()?;
-    println!("{}", std::str::from_utf8(&test.stdout)?);
-    println!("{}", std::str::from_utf8(&test.stderr)?);
+    let test = Command::new("cargo")
+        .args(["test", "-p", "norseline"])
+        .status()?;
 
     println!("Running `cargo audit`...");
-    let audit = Command::new("cargo").args(["audit"]).output()?;
-    println!("{}", std::str::from_utf8(&audit.stdout)?);
-    println!("{}", std::str::from_utf8(&audit.stderr)?);
+    let audit = Command::new("cargo").args(["audit"]).status()?;
 
     println!("Running `cargo fmt`...");
-    let fmt = Command::new("cargo").args(["fmt"]).output()?;
-    println!("{}", std::str::from_utf8(&fmt.stdout)?);
-    println!("{}", std::str::from_utf8(&fmt.stderr)?);
+    let fmt = Command::new("cargo").args(["fmt"]).status()?;
 
     println!("Running `cargo sqlx prepare --check -- --lib`...");
     let sqlx_prep = Command::new("cargo")
         .current_dir(project_root().join("norseline-rs"))
         .args(["sqlx", "prepare", "--check", "--", "--lib"])
-        .output()?;
+        .status()?;
 
-    println!("{}", std::str::from_utf8(&sqlx_prep.stdout)?);
-    println!("{}", std::str::from_utf8(&sqlx_prep.stderr)?);
+    print_error_with_status_code("cargo check", check);
+    print_error_with_status_code("cargo clippy", clippy);
+    print_error_with_status_code("cargo build", build);
+    print_error_with_status_code("cargo test", test);
+    print_error_with_status_code("cargo audit", audit);
+    print_error_with_status_code("cargo fmt", fmt);
+    print_error_with_status_code("cargo sqlx prepare", sqlx_prep);
 
     Ok(())
+}
+
+fn print_error_with_status_code(task: &str, status: ExitStatus) {
+    let code = match status.code() {
+        Some(x) => x.to_string(),
+        None => "<< no status code >>".to_string(),
+    };
+    if !status.success() {
+        println!(
+            "{} `{}` finished with a non-zero status code: {}",
+            "Error:".to_string().red(),
+            task.blue(),
+            code
+        );
+    }
 }
